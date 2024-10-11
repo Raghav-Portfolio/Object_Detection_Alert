dbc0050d105358dffd43a760f636cd89225d2d7b
Raghav-Git-Windows <rgs.rox@gmail.com>
2024-10-11 10:57:47 +0530

    Final Changes Implemented, Code is now ready

--- emailing.py
+++ emailing.py
@@ -1,2 +1,32 @@
-def send_email():
-    print('An email was sent!')
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+#Enter sender's email_ID:
+sender = ''
+#Enter your app password:
+password = '' 
+#Enter receiver's email_ID:
+receiver = ''
+
+
+def send_email(image_path):
+    print('send_email has started')
+    email_message = EmailMessage() # this object behaves like a dictionary
+    email_message['Subject'] = 'A new customer has entered'
+    email_message.set_content('Hey, we just got a new customer')
+    
+    with open(image_path, 'rb') as file:
+        content = file.read() 
+    
+    email_message.add_attachment(content, maintype = 'image', subtype = imghdr.what(None, content))
+    
+    gmail = smtplib.SMTP('smtp.gmail.com', port = 587) 
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(sender, password=password)
+    gmail.sendmail(sender, receiver, email_message.as_string())
+    gmail.quit()    
+    print('send_email has ended')
+if __name__ == '__main__':
+    send_email(image_path='images/19.png')
--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+import glob
+import os
 import cv2
 import time
 from emailing import send_email
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
+status_list = []
+count = 1
 
+def clean_folder():
+    print('clean_folder started')
+    images = glob.glob('images/*.png')
+    for image in images:
+        os.remove(image)
+    print('clean_folder has ended')
 while True:
     status = 0
-    status_list = []
-    
     check, frame = video.read()
     grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     grayscale_gaussian = cv2.GaussianBlur(grayscale, (21,21), 0) 
@@ -32,13 +41,24 @@ while True:
         rectangle = cv2.rectangle(frame, (x,y), (x+w,y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
-            
+            cv2.imwrite(f"images/{count}.png", frame)
+            count += 1
+            all_images = glob.glob('images/*.png')
+            # grab the frame in the middle from the time an object enters to the time they exit the video:
+            index = int(len(all_images)/2) 
+            image_with_object = all_images[index]
+        
     status_list.append(status)
-    print(status_list)
     status_list =  status_list[-2:]
     
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        email_thread = Thread(target=send_email, args=(image_with_object, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+        
+        email_thread.start()
+        
         
     cv2.imshow("My Video", frame)
     key = cv2.waitKey()
@@ -47,3 +67,4 @@ while True:
         break
 
 video.release()    
+clean_thread.start() # images should only be deleted after the press of the 'q' key
